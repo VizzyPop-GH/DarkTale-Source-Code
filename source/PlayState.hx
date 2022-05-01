93b0898c17c5e151dd47e13341757054dab73c5c
Zoni <85826200+timeless13GH@users.noreply.github.com>
2022-05-01 14:03:30 +0100

    initial engine rubbish

---
+++ source/PlayState.hx
@@ -0,0 +1,18 @@
+package;
+
+import flixel.FlxState;
+
+// ill actually do some code later
+// just want to get the bulk of the engine done fist
+class PlayState extends FlxState
+{
+	override public function create()
+	{
+		super.create();
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
